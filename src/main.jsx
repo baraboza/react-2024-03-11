f548f5a2fd9f446c33ad22b6af950f16bf39320b
Maxim Tkachev <maximtkachev99@gmail.com>
2024-03-24 11:46:30 +0300

    Дз1 - перенес папку constants в src

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import { restaurants } from '../constants/mock.js';
+import { restaurants } from './constants/mock.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
