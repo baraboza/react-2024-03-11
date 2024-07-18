2535133ee5e44cdeba049d585aa3adf6e99469ec
Maxim Tkachev <maximtkachev99@gmail.com>
2024-07-18 18:34:40 +0300

    Дз3 - storage utils

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,17 @@ import {Restaurant} from "./components/restaurant/component.jsx";
 import {Layout} from "./components/layout/components.jsx";
 import {useEffect, useState} from "react";
 import {RestaurantTabs} from "./components/restaurant-tabs/component.jsx";
-
-const getCurrentRestaurantIndexFromLS = () => {
-	return Number(localStorage.getItem('currentRestaurantIndex')) || 0
-}
-
-const saveCurrentRestaurantIndexToLS = (value) => {
-	localStorage.setItem('currentRestaurantIndex', value)
-}
+import {getStorageItem, setStorageItem} from "./utils/storage.js";
+import {STORAGE_KEYS} from "./constants/storage.js";
 
 export const App = () => {
-	const [currentRestaurantIndex, setCurrentRestaurantIndex] = useState(getCurrentRestaurantIndexFromLS)
+	const [currentRestaurantIndex, setCurrentRestaurantIndex] = useState(
+		() => Number(getStorageItem(STORAGE_KEYS.currentRestaurantIndex)) || 0
+	)
 	const currentRestaurant = restaurants[currentRestaurantIndex]
 
 	useEffect(() => {
-		saveCurrentRestaurantIndexToLS(currentRestaurantIndex)
+		setStorageItem(STORAGE_KEYS.currentRestaurantIndex, currentRestaurantIndex)
 	}, [currentRestaurantIndex])
 
 	return (
