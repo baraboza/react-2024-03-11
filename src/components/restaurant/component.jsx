abe8d82889d305595a04047c47c82a8c672f0970
Maxim Tkachev <maximtkachev99@gmail.com>
2024-07-22 12:11:18 +0300

    Дз4

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -1,7 +1,11 @@
 import {Menu} from "../menu/component.jsx";
 import {Reviews} from "../reviews/component.jsx";
+import {useCounter} from "../../hooks/useCounter.js";
 
 export const Restaurant = ({restaurant}) => {
+	const {amount, decrement, increment} = useCounter()
+
+
 	if (!restaurant) {
 		return null
 	}
@@ -11,6 +15,12 @@ export const Restaurant = ({restaurant}) => {
 	return (
 		<div>
 			<h2>{name}</h2>
+			<div>
+				<button onClick={decrement} disabled={amount === 0}>-</button>
+				<span>{amount}</span>
+				<button onClick={increment} disabled={amount === 5}>+</button>
+			</div>
+
 			<div>
 				<h3>Меню</h3>
 				<Menu menu={menu}/>
